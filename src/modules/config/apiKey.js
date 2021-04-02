3b444e165a893af2c45ae8ac29d639bec39a9653
BisleyDev <andrey.bisley@yahoo.com>
2021-04-02 12:09:54 +0300

    ForPortfolio

--- src/modules/config/apiKey.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const myAPI = {
-	KEY: 'd80CN7YeLAsZJo24YLJhnldzyt9VgER07lLGUw2Bbaiv7zzFwxFOfqruHu3CZvAu',
-	SECRET: 'b78mGX5sRENfuwjObeSr1HQCNmA3rsVGvpeUWQ04riYkdTByG6Hk5gNcoBNBSvAm',
-	KEYTELEGRAM: 'c3bcc0-2d2f8b-f71016',
-};
-module.exports = myAPI;
